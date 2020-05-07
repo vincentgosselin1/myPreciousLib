8c3ddc46140a90baec167012c93db69875ffeb1f
unknown <vincentgosselin1@gmail.com>
2020-05-06 21:54:04 -0400

    works with 19bits

--- parity_bit_calc_sm/simulation/MODELSIM_MACROS.tcl
+++ parity_bit_calc_sm/simulation/MODELSIM_MACROS.tcl
@@ -7,9 +7,13 @@
 # add wave -position insertpoint sim:/parity_bit_calc_sm_tb/i1/*
 
 ##16bit version
-vsim work.parity_bit_calc_sm_tb16
-add wave -position insertpoint sim:/parity_bit_calc_sm_tb16/i1/*
+# vsim work.parity_bit_calc_sm_tb16
+# add wave -position insertpoint sim:/parity_bit_calc_sm_tb16/i1/*
 #add wave
+
+##19bit version
+vsim work.parity_bit_calc_sm_tb19
+add wave -position insertpoint sim:/parity_bit_calc_sm_tb19/i1/*
 
 run 200 us
 #run 200000 us
