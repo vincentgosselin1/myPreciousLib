ba8649cd11c4dc7ddf4a8582b51516cf31ae41d9
unknown <vincentgosselin1@gmail.com>
2023-10-14 09:55:00 -0400

    with do files

---
+++ even_parity_detector/run_sim.do
@@ -0,0 +1,3 @@
+add wave -position insertpoint sim:/even_parity_detector_tb/*
+run 20 us
+exit
--- even_parity_detector/run_sim.sh
+++ even_parity_detector/run_sim.sh
@@ -1,5 +1,3 @@
 #run_sim.sh, Vincent Gosselin 2023.
 #needs to be configured once.
-vsim work.even_parity_detector_tb
-add wave -position insertpoint sim:/even_parity_detector_tb/*
-run 20 us
+vsim -do run_sim.do -wlf vsim.wlf work.even_parity_detector_tb
