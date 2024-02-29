2cf472f4dc55bafa93ec106da85739959c3e6b97
unknown <vincentgosselin1@gmail.com>
2024-02-28 20:14:37 -0500

    first class

---
+++ udemy_stuff/firt_class.sv
@@ -0,0 +1,16 @@
+class first;
+   bit x = 1'b1;
+endclass;
+
+module tb();
+
+   first f1;// Name of the instance
+
+   initial begin
+      f1 = new();      
+      $display("----------");
+      $display("The value of the x is %0b", f1.x);
+      $display("----------);
+end
+
+endmodule
