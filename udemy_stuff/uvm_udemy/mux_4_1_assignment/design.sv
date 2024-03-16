117fb0552d751720a698e7604464c9fc0fb4295e
vincentgosselin1 <vincentgosselin1@gmail.com>
2024-03-15 23:20:28 -0400

    s1

--- udemy_stuff/uvm_udemy/mux_4_1_assignment/design.sv
+++ udemy_stuff/uvm_udemy/mux_4_1_assignment/design.sv
@@ -1,3 +1,13 @@
+//                              -*- Mode: Verilog -*-
+// Filename        : design.sv
+// Description     : Mux4to1, input is 4bit wide : a,b,c,d. Selection is 2 bit wide, y is the output.
+// Author          : Vincent Gosselin
+// Created On      : Fri Mar 15 22:46:16 2024
+// Last Modified By: Vincent Gosselin
+// Last Modified On: Fri Mar 15 22:46:16 2024
+// Update Count    : 0
+// Status          : Unknown, Use with caution!
+
     module mux
       (
         input [3:0] a,b,c,d, ////input data port have size of 4-bit
