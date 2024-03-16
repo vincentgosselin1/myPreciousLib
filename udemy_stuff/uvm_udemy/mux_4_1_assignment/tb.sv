74bf818241c9e4f3d224047e9921e913cc3850f3
vincentgosselin1 <vincentgosselin1@gmail.com>
2024-03-16 00:14:05 -0400

    agent

--- udemy_stuff/uvm_udemy/mux_4_1_assignment/tb.sv
+++ udemy_stuff/uvm_udemy/mux_4_1_assignment/tb.sv
@@ -160,11 +160,37 @@ class scoreboard extends uvm_scoreboard;
       //if(y = '1')
    //test pass
    //else
-   //test fail
+   //test fail   
+endclass // scoreboard
+
+//////////////////////////////agent
+class agent extends uvm_agent;
+   `uvm_component_utils(agent)
    
    
+   function new(input string path = "agent", uvm_component parent = null);
+      super.new(path, parent);
+   endfunction
    
+   monitor m;
+   driver d;
+   uvm_sequencer #(transaction) seqr;
+   
+   
+   virtual function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      m = monitor::type_id::create("m",this);
+      d = driver::type_id::create("d",this);
+      seqr = uvm_sequencer #(transaction)::type_id::create("seqr",this);
+   endfunction
+   
+   
+   virtual function void connect_phase(uvm_phase phase);
+      super.connect_phase(phase);
+      d.seq_item_port.connect(seqr.seq_item_export);
+   endfunction
 endclass
+
 
 
 //////////////////////////////////////////////////////////////interface
