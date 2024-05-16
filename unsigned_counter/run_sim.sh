0a2cb618e86aff3e919ed61f2882249f9ecda234
vincentgosselin1 <vincentgosselin1@gmail.com>
2024-05-16 19:50:53 -0400

    things i need to do

--- unsigned_counter/run_sim.sh
+++ unsigned_counter/run_sim.sh
@@ -1,4 +1,5 @@
 #run_sim.sh, Vincent Gosselin 2023.
 #needs to be configured once.
+#need to update to a new simulator perhaps iverilog
 vsim -do run_sim.do -wlf vsim.wlf work.unsigned_counter_tb
 #sleep 5
